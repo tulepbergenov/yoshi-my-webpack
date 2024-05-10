bf7861fb24b692224851c1ef6a083a54b7f6d6f8
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-05-10 12:26:53 +0500

    build: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: process.env.BUILD_MODE,
+  mode: process.env.BUILD_MODE ?? "development",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
